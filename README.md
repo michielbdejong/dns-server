e69b343ec0551ed373cba8696ac1ece5aae0ca14
Michiel de Jong <mbdejong@mozilla.com>
2016-06-01 15:11:07 +0200

    use docker run -e

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Or with Docker:
 
 ````bash
 docker build -t pagekite-letsencrypt server/
-docker run -d --net=host pagekite-letsencrypt
+docker run -d --net=host -e SERVER=knilxof.org -e DOMAIN=box.knilxof.org pagekite-letsencrypt
 ````
 
 Run the tests (from the repo root):
--- server/run.sh
+++ server/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# DOMAIN=box.knilxof.org
-# SECRET=foxbox
-
-DOMAIN=box.useraddress.net
-SECRET=foxbox
-
-pagekite.py --isfrontend --ports=80,443 --protos=http,https,websocket --domain=http,https,websocket:*.$DOMAIN:$SECRET &
+pagekite.py --isfrontend --ports=80,443 --protos=http,https,websocket --domain=http,https,websocket:*.$DOMAIN --authdomain=$SERVER &
 
 node /app/index.js /app/test/fixtures/certs 53 5300 $DOMAIN
